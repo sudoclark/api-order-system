44e9fdc3b1b0dbd84d9eb47ca71c17d36ad079b4
sudoclark <cclaraoliveira21@gmail.com>
2026-04-29 21:04:24 -0300

    feat: implementando rotas dos produtos

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from models.Tables import User, Product, OrderItem, Order
 from database import db
 from utils.ApiUtils import ApiUtils
 
-# ===========================================
+# ==========================================================
 # CONFIGURAÇÃO - Configuração inicial da API
-# ===========================================
+# ==========================================================
 
 app = Flask(__name__)
 resp = ApiUtils.response
@@ -27,9 +27,9 @@ login_manager.login_view = "login"
 
 
 
-# ================================================
+# ==========================================================
 # AUTENTICAÇÃO - Rotas de autenticação do usuário
-# ================================================
+# ==========================================================
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -73,9 +73,9 @@ def logout():
 
 
 
-# =================================================
-# USER - Rotas relacionadas ao usuário no sistema
-# =================================================
+# ==========================================================
+# USER - Rotas relacionadas aos usuários no sistema
+# ==========================================================
 
 @app.route("/users", methods=["POST"])
 def create_user():
@@ -87,7 +87,7 @@ def create_user():
     username = response.get("username")
     password = response.get("password")
 
-    if not username or not password:
+    if not username and not password:
         return jsonify(resp("ERROR", "Credenciais inválidas.")), 400
     
     hashed = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
@@ -177,6 +177,114 @@ def delete_user(id):
     db.session.commit()
 
     return jsonify(resp("OK", "Usuário deletado com sucesso."))
+
+
+
+
+# ==========================================================
+# PRODUCTS - Rotas relacionadas aos produtos no sistema
+# ==========================================================
+
+@app.route("/products", methods=["POST"])
+@login_required
+def create_product():
+    response = request.json
+
+    if not response or "name" not in response or "price" not in response:
+        return jsonify(resp("ERROR", "Dados inválidos. Verifique as informações enviadas.")), 400
+    
+    name = response.get("name")
+    price = response.get("price")
+
+    if not name and not price:
+        return jsonify(resp("ERROR", "Dados inválidos, o nome e o valor do produto são obrigatórios.")), 400
+    
+    if not isinstance(price, (float, int)) or price <= 0:
+        return jsonify(resp("ERROR", "Valor inválido, o preço precisa ser um número positivo maior que zero.")), 400
+    
+    exists = Product.query.filter_by(name=name).first()
+
+    if exists:
+        return jsonify(resp("ERROR", "Produto já existe.")), 400
+    
+    product = Product(name=name, price=price)
+
+    db.session.add(product)
+    db.session.commit()
+
+    return jsonify(resp("OK", "Produto adicionado com sucesso.")), 201
+
+
+@app.route("/products", methods=["GET"])
+@login_required
+def get_products():
+    products = Product.query.all()
+
+    return jsonify(resp("OK", "Listagem obtida com sucesso.", products=[p.to_dict() for p in products]))
+
+
+@app.route("/products/<int:id>", methods=["GET"])
+@login_required
+def get_product_by_id(id):
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify(resp("ERROR", "Produto não encontrado.")), 404
+    
+    return jsonify(resp("OK", "Produto obtido com sucesso.", product=product.to_dict()))
+
+
+@app.route("/products/<int:id>", methods=["PATCH"])
+@login_required
+def update_product(id):
+    if current_user.role != "admin":
+        return jsonify(resp("ERROR", "Você não tem permissão para alterar os dados dos produtos, verifique com um admin.")), 403
+    
+    response = request.json
+
+    if not response:
+        return jsonify(resp("ERROR", "Dados inválidos, verifique as informações enviadas.")), 400
+    
+    price = response.get("price")
+
+    if price:
+        if not isinstance(price, (float, int)) or price < 0:
+            return jsonify(resp("ERROR", "O preço precisa ser um número positivo maior que zero.")), 400
+    
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify(resp("ERROR", "Produto não encontrado.")), 404
+    
+    fields = ["name", "price"]
+
+    for field in fields:
+        if field in response:
+            if not response[field]:
+                return jsonify(resp("ERROR", "Dados inválidos. Tem certeza que enviou nome ou preço do produto?")), 400
+            
+            setattr(product, field, response[field])
+
+    db.session.commit()
+
+    return jsonify(resp("OK", "Produto alterado com sucesso."))
+
+
+@app.route("/products/<int:id>", methods=["DELETE"])
+@login_required
+def delete_product(id):
+    if current_user.role != "admin":
+        return jsonify(resp("ERROR", "Você não tem permissão para deletar um produto. Verifique com um admin.")), 403
+    
+    product = Product.query.get(id)
+
+    if not product:
+        return jsonify(resp("ERROR", "Produto não encontrado.")), 404
+    
+    db.session.delete(product)
+    db.session.commit()
+
+    return jsonify(resp("OK", "Produto deletado com sucesso."))
 
 
 
